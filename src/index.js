7e7130c299148c3e9ea972e7bfb3bf0726b8021e
Ivan Sergeev <ahrejlok2007@ya.ru>
2021-11-25 15:42:45 +0500

    add adaptive

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 
 import icon from './images/icon.png'
 import './styles/main.scss'
+const allSelectedItems = document.getElementsByClassName('table__second-td_squade')
+const selectedItem = document.getElementById('selectAll');
+selectedItem.addEventListener('click', () => addSelected(allSelectedItems, selectedItem)
 
+)
+
+const addSelected = (list, mainSelector) => {
+    for (let item of list) {
+       item.classList.toggle('selected')
+    }
+    mainSelector.classList.toggle('selected')
+}
