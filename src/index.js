628129f7536639a26574795c7cf72da045421943
Ivan Sergeev <ahrejlok2007@ya.ru>
2021-11-29 00:37:39 +0500

    add lighting tds

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import './styles/main.scss'
-const allSelectedItems = document.getElementsByClassName('table__second-td_squade')
-const selectedItem = document.getElementById('selectAll');
-selectedItem.addEventListener('click', () => addSelected(allSelectedItems, selectedItem)
-
-)
+import './styles/main.scss';
+import { constList } from './js/consts';
+const {allOpenedTd, allSelectedItems, selectedItem} = constList;
 
 const addSelected = (list, mainSelector) => {
     for (let item of list) {
@@ -11,3 +8,32 @@ const addSelected = (list, mainSelector) => {
     }
     mainSelector.classList.toggle('selected')
 }
+
+
+// add or remove light for openedTd
+const toggleLightFunc = ( event) => {
+    let focusOpenedTd=''
+
+    //find tr where event happen
+    if(event.target.classList.contains('opened-td-picture')) { 
+        focusOpenedTd = event.target.parentNode.parentNode.cells;
+    }
+    // light td with the same img
+    for (var i = 0; i < focusOpenedTd.length; i++) {
+    if(event.target.src===focusOpenedTd[i].firstChild.src)
+    focusOpenedTd[i].classList.toggle('focused'); 
+    }
+}
+
+// choose all tr or remove
+selectedItem.addEventListener('click', () => addSelected(allSelectedItems, selectedItem));
+
+
+allOpenedTd.onmouseover = (event) => {
+    toggleLightFunc( event)
+}
+allOpenedTd.onmouseout = (event) => {
+    toggleLightFunc(event)
+}
+
+
---
+++ src/js/consts.js
@@ -0,0 +1,5 @@
+const allSelectedItems = document.getElementsByClassName('table__second-td_squade')
+const selectedItem = document.getElementById('selectAll');
+const allOpenedTd = document.getElementById('second-table');
+
+export const constList = {allOpenedTd, allSelectedItems, selectedItem}
